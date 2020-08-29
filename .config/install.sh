d54a68e17323539fdde6cff4471104bb5de8c9d9
nlantau <niklaslantau@gmail.com>
2020-08-30 00:31:08 +0200

    install.sh update; Linux adjustments

--- .config/install.sh
+++ .config/install.sh
@@ -7,9 +7,9 @@
 
 
 if [[ -f ./colors.sh ]]; then
-	source ./colors.sh
+  source ./colors.sh
 else
-	echo "Missing colors.sh. Output will be messed up"
+  echo "Missing colors.sh. Output will be messed up"
 fi
 
 # Start installation
@@ -20,28 +20,51 @@ echo -e "[${green}*${default}] Checking your system..."
 # Check OS
 
 if [[ "$(uname)" == "Darwin" ]]; then
-	echo -e "${underline}> Running on MacOS ${reset}\n"
+  echo -e "${underline}> Running on MacOS ${reset}\n"
+  
+  echo -e "[${green}*${default}] Checking if HomeBrew is installed..."
+  
+  # Check Brew
+  if test ! "$( command -v brew)"; then
+    echo -e "${underline}> Installing HomeBrew \n${reset}"
+    ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
 
-	echo -e "[${green}*${default}] Checking if HomeBrew is installed..."
-	
-	# Check Brew
-	if test ! "$( command -v brew)"; then
-		echo -e "${underline}> Installing HomeBrew \n${reset}"
-		ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+  elif test "$( command -v brew)"; then
+    echo -e "${underline}> HomeBrew already installed. Skipping...\n${reset}"
+  fi
 
-	elif test "$( command -v brew)"; then
-		echo -e "${underline}> HomeBrew already installed. Skipping...\n${reset}"
+  # Install Brewfile
+  echo -e "${underline}> Installing HomeBrew Bundle..\n${reset}"
+  #brew bundle install
 
-	fi
-
-		# Install Brewfile
-		echo -e "${underline}> Installing HomeBrew Bundle..\n${reset}"
-		#brew bundle install
-
-else
-	echo -e "${underline}> Install.sh only modified for MacOS. \n${reset}"
-	exit
-
+elif [[ "$(uname)" == "Linux" ]]; then
+  echo -e "${underline}> Running Linux  \n${reset}"
+  
+  echo -e "[${green}*${default}] Updating & Upgrading..."
+  sudo apt update
+  sudo apt -y upgrade
+  
+  echo -e "[${green}*${default}] Installing Python3..."
+  sudo apt install -y python3-pip
+  
+  echo -e "[${green}*${default}] Installing Build-Essentials..."
+  sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
+  
+  echo -e "[${green}*${default}] Installing Python3 venv..."
+  sudo apt install -y python3-venv
+  
+  # Venv directory
+  if ! [[ -d $HOME/.virtualenvs ]]; then
+    echo -e "[${green}*${default}] Creating directory for virtual environments..."
+    mkdir $HOME/.virtualenvs
+    cd $HOME/.virtualenvs
+    python3 -m venv base_env
+  fi 
+  
+  if [[ -f ./requirements.txt ]]; then
+    source $HOME/.virtualenvs/base_env/bin/active
+    pip3 install -r requirements.txt
+  fi
 fi
 
 
@@ -52,16 +75,16 @@ echo -e "[${green}*${default}] Checking current shell..."
 # Check current shell
 
 if [[ $SHELL == $(which zsh) ]]; then
-	SYS_SHELL="$(echo $SHELL | cut -d / -f 3 )"
-	echo -e "${underline}> Active shell: ${SYS_SHELL}\n${reset}" 
+  SYS_SHELL="$(echo $SHELL | cut -d / -f 3 )"
+  echo -e "${underline}> Active shell: ${SYS_SHELL}\n${reset}" 
 	
 # If current is bash, set zsh as default
 
 elif [[ $SHELL == $(which bash) ]]; then
-	echo -e "${underline}> Bash active...${reset}"
-	echo -e "> Changing to ZSH..."
-	chsh -s "$(command -v zsh)"
-	echo -e "${underline}> Default shell changed to ZSH\n${reset}"
+  echo -e "${underline}> Bash active...${reset}"
+  echo -e "> Changing to ZSH..."
+  chsh -s "$(command -v zsh)"
+  echo -e "${underline}> Default shell changed to ZSH\n${reset}"
 
 fi
 
@@ -73,24 +96,24 @@ fi
 echo -e "[${green}*${default}] Checking if virtualenvs directory exists..."
 
 if ! [[ -d $HOME/.virtualenvs ]]; then
-	echo -e "${underline}> Creating virtualenvs directory...${reset}"
-	mkdir -p ~/.virtualenvs
-	echo -e "> Done\n"
-	# Check venv 
-	# if no venv, create venv and pip install neovim dependencies
+  echo -e "${underline}> Creating virtualenvs directory...${reset}"
+  mkdir -p ~/.virtualenvs
+  echo -e "> Done\n"
+  # Check venv 
+  # if no venv, create venv and pip install neovim dependencies
 
 else
-	echo -e "${underline}> ${VIRDIR} already exists. Skipping...\n${reset}"
-	echo -e "[${green}*${default}] Checking existing venvs..."
-	echo -e "> Current venvs in $VIRDIR:"
-	
-	# Print all virtual environments
-
-	for dir in $folders; do
-		echo -e "> $dir"
-
-	done
-	echo -e ''
+  echo -e "${underline}> ${VIRDIR} already exists. Skipping...\n${reset}"
+  echo -e "[${green}*${default}] Checking existing venvs..."
+  echo -e "> Current venvs in $VIRDIR:"
+  
+  # Print all virtual environments
+  
+  for dir in $folders; do
+    echo -e "> $dir"
+  
+  done
+  echo -e ''
 
 fi
 
